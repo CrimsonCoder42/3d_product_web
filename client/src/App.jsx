3f4015fe2ad065d1e52b66ae642c70f086f52ac6
CrimsonCoder42 <nostro37@gmail.com>
2023-09-20 11:45:28 -0400

    Fixed a bug in the login module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 
 
+import Canvas from './canvas'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
+
 function App() {
 
   return (
-    <>
+    <main className='app transition-all ease-in'>
       <div>
-        <h1 className="head-text">React App</h1>
+        <Home />
+        <Canvas />
+        <Customizer />
         
+
       </div>
-    </>
+    </main>
   )
 }
 
